c95faea3fcf1f131893b5af3da92686500b68427
daniel.szopa333 <daniel.szopa333@gmail.com>
2022-04-13 11:56:46 +0200

    Create Singleton

---
+++ Signleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using Signleton;
+
+
+
+var cfg = SqlConfiguration.GetInstance();
+cfg.Server = "localhost";
+cfg.Database = "ErpDatabase";
+cfg.User = "sa";
+cfg.Password = "pass";
+
+var cfg2 = SqlConfiguration.GetInstance();
+
+var result = ReferenceEquals(cfg, cfg2);
+var resultMessage = result ? "Configuration is Singleton" : "Configuration is not Singleton";
+Console.WriteLine(resultMessage);
---
+++ Signleton/SqlConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace Signleton
+{
+    public class SqlConfiguration
+    {
+        private static SqlConfiguration? _instance = null;
+        private static object objLock = new object();
+
+        public string? Server { get; set; }
+        public string? Database { get; set; }
+        public string? User { get; set; }
+        public string? Password { get; set; }
+
+        private SqlConfiguration()
+        {
+
+        }
+
+        public static SqlConfiguration GetInstance()
+        {
+            lock(objLock)
+            {
+                if (_instance is null)
+                {
+                    _instance = new SqlConfiguration();
+                }
+            }
+           
+            return _instance;
+        }
+    }
+}
